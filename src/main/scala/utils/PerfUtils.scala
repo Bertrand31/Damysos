59aa96531edc46bb4e3393b13b1b777e2098a934
Bertrand31 <bertrandjun@gmail.com>
2019-05-14 18:25:10 +0800

    Improve PerfUtils

--- src/main/scala/utils/PerfUtils.scala
+++ src/main/scala/utils/PerfUtils.scala
@@ -10,4 +10,32 @@ object PerfUtils {
     println(s"$id took $elapsedStr")
     result
   }
+
+  def profile(id: String)(block: => Any): Long = {
+    println("============================")
+    println(s"Profiling $id:")
+    def print_result(s: String, ns: Long) = {
+      val format = java.text.NumberFormat.getIntegerInstance.format(_: Long)
+      println(s.padTo(16, " ").mkString + format(ns) + " ns")
+    }
+
+    var t0 = System.nanoTime()
+    var result = block
+    var t1 = System.nanoTime()
+
+    print_result("Cold run", (t1 - t0))
+
+    val runtimes = (1 to 10).map(i => {
+      t0 = System.nanoTime()
+      result = block
+      t1 = System.nanoTime()
+      (t1 - t0).toLong
+    })
+
+    print_result("Max", runtimes.max)
+    print_result("Min", runtimes.min)
+    val avg = runtimes.sum / runtimes.length
+    print_result("Avg", avg)
+    avg
+  }
 }
