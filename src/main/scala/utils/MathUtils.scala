f44be2a615d1018718731ab275b2c195695e4dde
Bertrand31 <bertrandjun@gmail.com>
2020-01-22 21:37:31 +0100

    Improve toBase speed by using Java Integer method

--- src/main/scala/utils/MathUtils.scala
+++ src/main/scala/utils/MathUtils.scala
@@ -1,6 +1,8 @@
 package utils
 
+import java.lang.{Long => JavaLong}
+
 object MathUtils {
 
-  def toBase(base: Int, number: Long): String = BigInt(number).toString(base)
+  def toBase(base: Int, number: Int): String = JavaLong.toString(number, base)
 }
